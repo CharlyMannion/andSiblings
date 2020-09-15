a20ff8f96473e1c88851cf0fbf7516a96471ebae
CharlyMannion <charlottemannion100@gmail.com>
2020-09-15 17:51:40 +0100

    TTDs solution, but returning an array as opposed to a string

--- andSibling.js
+++ andSibling.js
@@ -1,5 +1,21 @@
 function andSibling(input) {
-    return input;
+    if (input.length < 2) {
+        return input;
+    } else if (!input || typeof input !== "string") {
+        return "Invalid Input: Please enter a string!";
+    }
+    const results = [];
+
+    for (let i = 0; i < input.length; i++) {
+        const firstChar = input[i];
+        const otherChar = input.substring(0, i) + input.substring(i + 1);
+        const otherPermutations = andSibling(otherChar);
+
+        for (let j = 0; j < otherPermutations.length; j++) {
+            results.push(firstChar + otherPermutations[j]);
+        }
+    }
+    return results;
 };
 
 module.exports = andSibling;
--- spec/andSiblings.spec.js
+++ spec/andSiblings.spec.js
@@ -4,6 +4,22 @@ describe('andSibling', () => {
     it("returns an empty string when passed an empty string", () => {
         const inputStr = "";
         const expected = "";
+        expect(andSibling(inputStr)).toEqual(expected);
+    });
+    it("returns an error if passed an input that isn\'t a string", () => {
+        const inputStr = ["not", "a", "string", 2];
+        const expected = "Invalid Input: Please enter a string!";
         expect(andSibling(inputStr)).toBe(expected);
     });
+    it("returns the input string when the string.length < 2", () => {
+        const inputStr = "a";
+        const expected = "a";
+        expect(andSibling(inputStr)).toEqual(expected);
+    });
+    it("retuns the AND-siblings when the string.length >= 2", () => {
+        const inputStr = "236";
+        const expected = "632,623,362,326,263,236";
+        expect(andSibling(inputStr)).toEqual(expected);
+        expect(typeof andSibling(inputStr)).toBe("string");
+    })
 })
