e6ca5c0fe78c45af008189fb2bb9623b58548354
CharlyMannion <charlottemannion100@gmail.com>
2020-09-17 10:00:15 +0100

    fixes problem by making substr back into an array before calling forEach

--- andSibling.js
+++ andSibling.js
@@ -1,22 +1,24 @@
 function andSibling(input) {
     const results = [];
-
+    if (input.length === 2) {
+        return [input, input[1] + input[0]].join();
+    }
+    // move to top 
     if (input.length < 2) {
         return input;
-    } else if (input.length === 2) {
-        return [input, input[1] + input[0]];
     } else if (!input || typeof input !== "string") {
         return "Invalid Input: Please enter a string!";
-    }
-    input.split('').forEach(function(char, index, arr) {
-        var subStr = [].concat(arr);
-        subStr.splice(index, 1);
-        andSibling(subStr.join('')).forEach(function(elem) {
-            results.push(char + elem);
+    } else {
+        input.split('').forEach(function(char, index, arr) {
+            var subStr = [].concat(arr);
+            subStr.splice(index, 1);
+            andSibling(subStr.join('')).split(',').forEach(function(elem) {
+                results.push(char + elem);
+            });
         });
-    });
-
-    return results.join();
+        // filter unique values in array before returning it
+        return results.join();
+    }
 };
 
 module.exports = andSibling;
