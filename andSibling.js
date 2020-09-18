8f004740aa6d42b5dc93b1ff597e4a4db313098c
CharlyMannion <charlottemannion100@gmail.com>
2020-09-18 08:40:28 +0100

    further refactors for readability

--- andSibling.js
+++ andSibling.js
@@ -1,27 +1,23 @@
 const andSibling = (input) => {
+    const resultSiblings = [];
     const filteredStr = input.replace(/\D/g, '');
-    const results = [];
-    if (input.length < 1) {
-        return input;
-    }
-    if (filteredStr.length < 1) {
-        return "Invalid Input: Please enter a string that contains a numeric character!";
-    }
-    if (filteredStr.length < 2) {
-        return filteredStr;
-    }
-    if (filteredStr.length === 2) {
-        return [filteredStr, filteredStr[1] + filteredStr[0]].join();
-    }
+    const errorMsg = "Invalid Input: Please enter a string that contains a numeric character!"
+    const arrForTwoChars = [filteredStr, filteredStr[1] + filteredStr[0]].join()
+
+    if (input.length < 1) return input;
+    if (filteredStr.length < 1) return errorMsg;
+    if (filteredStr.length < 2) return filteredStr;
+    if (filteredStr.length === 2) return arrForTwoChars;
+
     filteredStr.split('').forEach((char, index, arr) => {
         var subStr = [].concat(arr);
         subStr.splice(index, 1);
         andSibling(subStr.join('')).split(',').forEach((elem) => {
-            results.push(char + elem);
+            resultSiblings.push(char + elem);
         });
     });
-    const unique = [...new Set(results)];
-    return unique.join();
+    const uniqueSiblings = [...new Set(resultSiblings)].join();
+    return uniqueSiblings;
 };
 
 module.exports = andSibling;
